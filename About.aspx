1b70db53da5585777cb467090e920c5383b4e100
Stephen Moore <77762551+stephenmoore33@users.noreply.github.com>
2022-12-10 09:46:41 -0500

    Add project files.

---
+++ About.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="About" Language="VB" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.vb" Inherits="WebApplication1.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.<asp:Login ID="Login1" runat="server">
+        </asp:Login>
+    </h2>
+    <p>Your app description page.</p>
+    <p>Use this area to provide additional information.</p>
+</asp:Content>
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("WebApplication1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("SMOORETECH")>
+<Assembly: AssemblyProduct("WebApplication1")>
+<Assembly: AssemblyCopyright("Copyright © SMOORETECH 2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ca1b4d5c-7b3b-487d-9756-2c9384895595")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
